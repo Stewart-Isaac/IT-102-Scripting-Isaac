130ec502ffe5f90229cb8b7aaadba47de0867032
Stewart-Isaac <stewart.isaac@student.greenriver.edu>
2025-10-05 21:52:51 -0700

    Updated the hello world script to ask for name

--- end/CH02/HelloWorld.py
+++ end/CH02/HelloWorld.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
-# Created by Ed Goad, 2/3
+# Created by Isaac Stewart
 print("Hello World")
+
+#prompts the user for their name
+your_name = input("Please enter your name: ")
+print("Hello ", your_name)
+if your_name == "Isaac":
+    print("Nice name dummy")
+print("Today is going to be a great day")
+
+age = input("Please enter your age: ")
+age2years = int(age) + 2
+print("You will be", age2years, "in 2 years")
