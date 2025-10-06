9a0e1ef720854cb358c0cb6b6ba9aadf23444286
Stewart-Isaac <stewart.isaac@student.greenriver.edu>
2025-10-06 16:22:32 -0700

    Made conditional with loop to repeat message

--- end/CH03/Conditionals.py
+++ end/CH03/Conditionals.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
 # example workign with conditionals
-#By Ed Goad
-# date: 2/3/2021
+#By Isaac Stewart
 
-# Suggestion - 
-#   add in <, ==, > one at a time
-#   make each of them if statements initially
-#   Change x and y values to test the various paths
-#   eventually simplfy with if, elif, else
 question = 0
 while question == 0:
     dayQuality = input("is today a good day? (y/n): ")
