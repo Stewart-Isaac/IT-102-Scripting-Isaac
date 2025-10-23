d0e40c7bff8770c9794e89bed7965bbb3f775d44
Stewart-Isaac <stewart.isaac@student.greenriver.edu>
2025-10-22 21:10:39 -0700

    changed the funcy file

--- end/CH03/Funcy.py
+++ end/CH03/Funcy.py
@@ -1,37 +1,20 @@
 #!/usr/bin/env python3
-# example workign with Functions
-#By Ed Goad
-# date: 2/3/2021
+# example workign with Loops
+##By Isaac Stewart
 
-# Suggestions - 
-#  build out 1 function at a time, from top down
-#  comment out calls to functions that arent needed to keep the output clean
-#  When finished, uncomment all calls, then put a breakpoint at first call
-#  Use debugging to show difference between step-over, step-into, step-out
+def send_message():
+    for x in range(10):
+            print("yeah it is :)")
+    
 
-# Basic function example
-def func1():
-    print("I am a function")
-
-# Function that adds numbers
-def addNums(val1, val2):
-    print(val1 + val2)
-
-# Function that raises to exponent, default to squared
-def power(number, exp=2):
-    print(number ** exp)
-
-# Fucntion that returns value
-def cube(x):
-    return x**3
-
-func1()
-addNums(2, 5)
-power(5, 3)
-power(5)
-value = cube(5)
-print("The answer is {0}".format(value))
-power(value, 5)
-
-
-print("The answer is {0}".format(cube(6)))
+question = 0
+while question == 0:
+    dayQuality = input("is today a good day? (y/n): ")
+    if dayQuality == "y":
+        send_message()
+        question = 1
+    elif dayQuality == "n":
+        print("sorry to hear that")
+        question = 1
+    else:
+        print("Input not recognized please try again")
